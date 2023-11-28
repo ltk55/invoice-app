da29ae740b16a30ee326e1f9b281044ec03d0ae8
Frank Lam <frankltk55@gmail.com>
2023-11-28 17:15:24 +0000

    style: align items, outline and go-to arrow

--- components/home/InvoiceItem.tsx
+++ components/home/InvoiceItem.tsx
@@ -1,4 +1,7 @@
+import Image from "next/image";
+
 import { formatCurrency, formatDate } from "@/lib/utils";
+import iconArrowRight from "@/public/assets/icon-arrow-right.svg";
 import { type Invoice } from "@/types";
 
 import StatusBadge from "../shared/StatusBadge";
@@ -11,14 +14,14 @@ export default function InvoiceItem({
   const { id, paymentDue, total, clientName, status } = invoice;
 
   return (
-    <li className="flex h-[134px] w-[327px] items-center justify-between rounded-lg bg-white px-6 pb-[22px] pt-6 shadow dark:bg-colour-300 md:flex md:h-[72px] md:w-[672px] md:justify-between md:px-8 md:py-0">
+    <li className="flex h-[134px] w-[327px] cursor-pointer items-center justify-between rounded-lg bg-white px-6 pb-[22px] pt-6 shadow hover:outline hover:outline-colour-100 dark:bg-colour-300 md:flex md:h-[72px] md:w-[672px] md:justify-start md:px-8 md:py-0">
       <div className="flex h-full flex-col items-start justify-between md:flex md:flex-row md:items-center">
-        <div className="font-bold leading-none md:mr-7">
+        <div className="font-bold leading-none md:mr-2 md:w-20">
           <span className="text-colour-700">#</span>
           <span className="text-colour-800 dark:text-white">{id}</span>
         </div>
 
-        <div className="md:mr-[53px]">
+        <div className="md:mr-3 md:w-32">
           <span className="text-xs font-medium leading-none text-colour-600 dark:text-colour-500">
             Due{" "}
           </span>
@@ -31,22 +34,28 @@ export default function InvoiceItem({
           {formatCurrency(total)}
         </div>
 
-        <div className="hidden text-xs font-medium leading-none text-slate-400 dark:text-white md:mr-[75px] md:block">
+        <div className="hidden text-xs font-medium leading-none text-slate-400 dark:text-white md:mr-[10px] md:block md:w-28">
           {clientName}
         </div>
       </div>
 
-      <div className="flex h-full flex-col items-center justify-between md:flex md:flex-row">
+      <div className="flex h-full flex-col items-center justify-between md:mr-5 md:flex md:flex-row">
         <div className="text-right text-xs font-medium leading-none text-slate-400 dark:text-white md:hidden">
           {clientName}
         </div>
 
-        <div className="hidden font-bold dark:text-white md:mr-10 md:block">
+        <div className="hidden font-bold dark:text-white md:mr-10 md:block md:w-[90px] md:text-right">
           {formatCurrency(total)}
         </div>
 
         <StatusBadge status={status} className="mb-[5px] md:mb-0" />
       </div>
+
+      <Image
+        src={iconArrowRight}
+        alt={`go to invoice ${id}`}
+        className="hidden md:block"
+      />
     </li>
   );
 }
