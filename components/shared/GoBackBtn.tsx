f53c59a7b3f9aad125a612a4044e821702d2f28b
Frank Lam <frankltk55@gmail.com>
2023-11-29 16:44:02 +0000

    feat: add go back button

---
+++ components/shared/GoBackBtn.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+import iconArrowLeft from "@/public/assets/icon-arrow-left.svg";
+
+export default function GoBackBtn(): JSX.Element {
+  const router = useRouter();
+
+  return (
+    <button
+      onClick={() => {
+        router.back();
+      }}
+      className="flex items-center gap-4"
+    >
+      <Image src={iconArrowLeft} alt="go back" />
+      <span className="text-xs font-bold text-colour-800 dark:text-white">
+        Go Back
+      </span>
+    </button>
+  );
+}
